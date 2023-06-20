27bd11861b2ac9908321281509e462fbfe0ca238
marceop <marta.op18@gmail.com>
2023-06-20 12:04:49 +0200

    Feat: importing routes and different views from pages

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import './App.css'
-import { Header } from './common/Header/Header';
-import { Body } from './pages/Body/Body';
 
+import React from 'react';
 
-function App() {
-
-  return (
-    <>
-
-      <Header />
-      <Body />
-      
-    </>
-  )
+import {Route, Routes, Navigate} from 'react-router-dom';
+import { Home } from '../Home/Home';
+import { Login } from '../Login/Login';
+import { Register } from '../Register/Register';
+ 
+export const Body = () => {
+     return (
+        <> 
+            <Routes>
+                <Route path="*" element={<Navigate to="/"/>}/>
+                <Route path="/" element={<Home />}/> 
+                <Route path="/login" element={<Login />}/>
+                <Route path="/register" element={<Register />}/>
+            </Routes>
+        </> 
+     )
 }
 
-export default App
+// si algo no existe nos lleva a home: 
+// <Route path="/" element={<Home />}/> 
