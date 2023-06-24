467f8dbd3ab83833636e6e4522f3f227c8252d69
marceop <marta.op18@gmail.com>
2023-06-24 08:53:37 +0200

    defining changeview function to navigate

---
+++ src/common/ChangeView/Changeview.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './ChangeView.css';
+import { useNavigate } from "react-router-dom";
+
+export const ChangeView = ({path, name}) => {
+
+    const navigate = useNavigate();
+
+    return(
+        <div className="changeViewDesign" onClick={()=>navigate(path)}>
+            {name}
+        </div>
+    )
+}
